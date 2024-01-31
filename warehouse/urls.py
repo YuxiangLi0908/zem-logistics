55c6a8a8d3caa4424474c86457c6efc293d98df1
Yuxiang LI <yuxiang.li@shipt.com>
2024-01-30 21:40:53 -0500

    static file

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -4,6 +4,8 @@ from warehouse.views.user_login import *
 from warehouse.views.order_creation import OrderCreation
 from warehouse.views.retrieval_schedule import ScheduleRetrieval
 from warehouse.views.palletization import Palletization
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", home, name="home"),
@@ -17,4 +19,4 @@ urlpatterns = [
     # path('schedule_shipment/', views.ScheduleShipment.as_view(), name='schedule_shipment'),
     # path('schedule_shipment/<str:warehouse>/<str:destination>/', views.ScheduleShipment.as_view(), name='schedule_shipment'),
     # path('outbound/', views.Outbound.as_view(), name='outbound'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
