3ccb476f72b1907fe9128caae39982c3f07ad02f
Yuxiang LI <yuxiang.li@shipt.com>
2024-03-14 21:59:55 -0400

    update

--- warehouse/views/export_file.py
+++ warehouse/views/export_file.py
@@ -43,33 +43,3 @@ def export_bol(context: dict[str, Any]):
     if pisa_status.err:
         raise ValueError('Error during PDF generation: %s' % pisa_status.err, content_type='text/plain')
     return response
-
-# def generate_pdf(request):
-#     # Your HTML template file path
-#     template_path = 'export_file/do.html'
-#     # raise ValueError(f"{os.listdir(template_path)}")
-    
-#     # Load the HTML template
-#     try:
-#         template = get_template(template_path)
-#     except:
-#         raise ValueError("no template")
-    
-#     # Context data to be passed to the template
-#     context = {'sample_data': 'Hello, this is some sample data!'}
-    
-#     # Render the template with the context data
-#     html = template.render(context)
-    
-#     # Create a PDF file
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="output.pdf"'
-    
-#     # Generate PDF using xhtml2pdf
-#     pisa_status = pisa.CreatePDF(html, dest=response)
-    
-#     # Check if PDF generation was successful
-#     if pisa_status.err:
-#         raise ValueError('Error during PDF generation: %s' % pisa_status.err, content_type='text/plain')
-    
-#     return response
