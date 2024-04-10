f49d5ee8839ace967e7c8fd284f27af567cec314
Yuxiang LI <yuxiang.li@shipt.com>
2024-04-10 00:11:39 -0400

    update customer management

--- warehouse/views/customer_management.py
+++ warehouse/views/customer_management.py
@@ -15,31 +15,66 @@ from warehouse.forms.customer_form import CustomerForm
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class CustomerManagement(View):
     template_main = "new_customer.html"
-    context: dict[str, Any] = {}
 
-    def get(self, request: HttpRequest) -> HttpResponse:
+    def get(self, request: HttpRequest, **kwargs) -> HttpResponse:
+        customer_name = kwargs.get("name", None)
         step = request.GET.get("step")
-        if step == "new":
-            return self.handle_new_customer_get(request)
+        if step =="update":
+            return render(request, self.template_main, self.handle_customer_update_get(request, customer_name))
         else:
-            raise ValueError(f"{request}")
+            return render(request, self.template_main, self.handle_all_customer_get(request))
     
-    def post(self, request:HttpRequest) -> HttpResponse:
-        step = request.GET.get("step")
+    def post(self, request:HttpRequest, **kwargs) -> HttpResponse:
+        step = request.POST.get("step")
+        customer_name = kwargs.get("name", None)
         if step == "new":
-            return self.handle_new_customer_post(request)
-        else:
-            raise ValueError(f"{request}")
+            return render(request, self.template_main, self.handle_new_customer_post(request))
+        elif step == "update":
+            self.handle_customer_update_post(request, customer_name)
+            return redirect("customer_management")
+        
+    def handle_all_customer_get(self, request: HttpRequest) -> dict[str, Any]:
+        existing_customers = Customer.objects.all().order_by("zem_name")
+        context = {
+            "existing_customers": existing_customers,
+            "customer_form": CustomerForm(),
+        }
+        return context
     
-    def handle_new_customer_get(self, request: HttpRequest) -> HttpResponse:
-        self.context["customer_form"] = CustomerForm()
-        return render(request, self.template_main, self.context)
+    def handle_customer_update_get(self, request: HttpRequest, customer_name: str) -> dict[str, Any]:
+        customer = Customer.objects.get(zem_name=customer_name)
+        customer_update_form = CustomerForm(instance=customer)
+        context = {
+            "customer_update_form": customer_update_form,
+        }
+        return context
     
-    def handle_new_customer_post(self, request: HttpRequest) -> HttpResponse:
+    def handle_new_customer_post(self, request: HttpRequest) -> dict[str, Any]:
         form = CustomerForm(request.POST)
+        existing_customers = Customer.objects.all()
+        zem_names = [c.zem_name for c in existing_customers]
+        full_names = [c.full_name for c in existing_customers]
         if form.is_valid():
-            form.save()
-            return redirect('home')
+            if form.cleaned_data["full_name"] not in full_names and form.cleaned_data["zem_name"] not in zem_names:
+                form.save()
+                context = self.handle_all_customer_get(request)
+                return context
+            else:
+                context = self.handle_all_customer_get(request)
+                context.update({
+                    "customer_form": form,
+                    "duplicated": True,
+                })
+                return context
         else:
             raise ValueError(f"{request}")
         
+    def handle_customer_update_post(self, request: HttpRequest, customer_name: str) -> None:
+        form = CustomerForm(request.POST)
+        selected_customer = Customer.objects.get(zem_name=customer_name)
+        if form.is_valid():
+            selected_customer.zem_name = form.cleaned_data.get("zem_name")
+            selected_customer.full_name = form.cleaned_data.get("full_name")
+            selected_customer.save()
+        else:
+            raise ValueError(f"invalid customer info")
