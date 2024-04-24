f2f1f9f16dee166066153113a4c1ab9062b8afe8
Yuxiang LI <yuxiang.li@shipt.com>
2024-04-24 00:20:00 -0400

    update

--- warehouse/models/customer.py
+++ warehouse/models/customer.py
@@ -7,4 +7,4 @@ class Customer(models.Model):
     zem_code = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.zem_name} - {self.full_name}"
+        return f"{self.zem_name}"
