827203ebe2d5d328471419678750f83a1d861540
Yuxiang LI <yuxiang.li@shipt.com>
2024-05-20 13:36:17 -0400

    add fee to model

--- warehouse/models/offload.py
+++ warehouse/models/offload.py
@@ -5,6 +5,10 @@ class Offload(models.Model):
     offload_required = models.BooleanField(default=True)
     offload_at = models.DateTimeField(null=True, blank=True)
     total_pallet = models.IntegerField(null=True, blank=True)
+    devanning_company = models.CharField(max_length=100, null=True, blank=True)
+    devanning_fee = models.FloatField(null=True, blank=True)
+    devanning_fee_paid_at = models.DateField(null=True, blank=True)
+    is_devanning_fee_paid = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.offload_id
