7db943afa4ac1cb65b287850e7dedd5f0985007d
Yuxiang LI <yuxiang.li@shipt.com>
2024-07-22 14:16:38 -0400

    remove redundant order_by

--- warehouse/views/quote_management.py
+++ warehouse/views/quote_management.py
@@ -146,9 +146,7 @@ class QuoteManagement(View):
         else:
             default_date = datetime.now().date() + timedelta(days=-30)
             criteria = models.Q(created_at__gte=default_date)
-        quote = Quote.objects.filter(criteria).order_by(
-            "-created_at", "customer_name", "parent_id", "warehouse", "platform", "load_type", "is_lift_gate"
-        )
+        quote = Quote.objects.filter(criteria).order_by("-created_at")
         context = {
             "start_date": start_date,
             "end_date": end_date,
