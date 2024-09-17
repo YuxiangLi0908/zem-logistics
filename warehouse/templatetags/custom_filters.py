682daa18b9bdeb7add912c80d7df523729a243e3
Yuxiang LI <yuxiang.li@shipt.com>
2024-09-16 23:47:10 -0400

    add custom filter

--- warehouse/templatetags/custom_filters.py
+++ warehouse/templatetags/custom_filters.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import template
 
 register = template.Library()
@@ -5,3 +6,7 @@ register = template.Library()
 @register.filter
 def split_and_get_first(value: str) -> str:
     return value.split("-")[0]
+
+@register.filter(name='get')
+def get(dictionary: dict, key: Any) -> Any:
+    return dictionary.get(key)
