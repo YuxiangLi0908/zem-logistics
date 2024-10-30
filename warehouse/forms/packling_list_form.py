68b1f7d53469612f25b15a2836841d28d33d350d
Yuxiang LI <yuxiang.li@shipt.com>
2024-10-30 19:19:13 -0400

    fix palletization with 0 pcs

--- warehouse/forms/packling_list_form.py
+++ warehouse/forms/packling_list_form.py
@@ -9,7 +9,7 @@ class PackingListForm(forms.ModelForm):
         fields = "__all__"
         widgets = {
             "delivery_method": forms.Select(choices=DELIVERY_METHOD_OPTIONS),
-            "n_pallet": forms.NumberInput(attrs={"min": 1}),
+            "n_pallet": forms.NumberInput(attrs={"min": 0}),
         }
 
     def __init__(self, *args, **kwargs):
