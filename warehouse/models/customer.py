2a2f83eae7a48eea4f9c7d7d4842d0d002162dcd
Yuxiang LI <yuxiang.li@shipt.com>
2025-03-06 15:34:02 -0500

    add usr and pwd to customer

--- warehouse/models/customer.py
+++ warehouse/models/customer.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
 from simple_history.models import HistoricalRecords
 
 
@@ -11,12 +13,22 @@ class Customer(models.Model):
     phone = models.CharField(max_length=30, null=True, blank=True)
     note = models.CharField(max_length=500, null=True, blank=True)
     address = models.CharField(max_length=500, null=True, blank=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=255)
     history = HistoricalRecords()
 
     class Meta:
         indexes = [
             models.Index(fields=["zem_name"]),
         ]
+
+    def set_password(self, raw_password: str) -> None:
+        """Hashes and stores the password."""
+        self.hashed_password = make_password(raw_password)
+
+    def check_password(self, raw_password: str) -> str:
+        """Checks if the given password matches the stored hash."""
+        return check_password(raw_password, self.hashed_password)
 
     def __str__(self) -> str:
         return f"{self.zem_name}" if self.zem_name else f"{self.full_name}"
