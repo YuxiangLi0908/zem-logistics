a8d030609b858ea8163a4f8df519fc185ba6660c
sangwei <1178424577@qq.com>
2025-07-18 11:46:40 +0800

    本地派送的加个备注

--- warehouse/views/accounting.py
+++ warehouse/views/accounting.py
@@ -2777,14 +2777,8 @@ class Accounting(View):
             plt_ids = request.POST.getlist("plt_ids")
             new_plt_ids = [ast.literal_eval(sub_plt_id) for sub_plt_id in plt_ids]
             expense = request.POST.getlist("expense")
-            if (
-                type_value == "selfdelivery"
-                or type_value == "selfpickup"
-                or type_value == "upsdelivery"
-            ):  # 自发的要加备注
-                note = request.POST.getlist("note")
-            else:
-                note = None
+            #有的类型有备注，有的没有
+            note = request.POST.getlist("note") if "note" in request.POST else None
             # 将前端的每一条记录存为invoice_delivery的一条
             for i in range(len((new_plt_ids))):
                 ids = [int(id) for id in new_plt_ids[i]]
