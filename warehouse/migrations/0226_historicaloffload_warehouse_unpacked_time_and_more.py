c0f59d39140e93e5e3903e3cbd18df16d3159d4e
sangwei <1178424577@qq.com>
2025-09-15 10:41:07 +0800

    修改迁移文件

--- warehouse/migrations/0226_historicaloffload_warehouse_unpacked_time_and_more.py
+++ warehouse/migrations/0226_historicaloffload_warehouse_unpacked_time_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("warehouse", "0226_historicalpackinglist_express_number_and_more"),
+        ("warehouse", "0225_historicalpackinglist_express_number_and_more"),
     ]
 
     operations = [
