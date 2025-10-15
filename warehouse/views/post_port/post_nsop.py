2ac769ee6fe57e8c45cfc32c9b3d477b00f63f6f
sangwei <1178424577@qq.com>
2025-10-15 10:43:56 +0800

    1

--- warehouse/views/post_port/post_nsop.py
+++ warehouse/views/post_port/post_nsop.py
@@ -1505,15 +1505,15 @@ class PostNsop(View):
         auto_matches = await self.get_auto_matches(unshipment_pos, shipments)
         
         vessel_names = []
-        vessel_combined = []
+        vessel_dict = {} 
         for item in unshipment_pos:
             vessel_name = item.get('vessel_name')
             vessel_eta = item.get('vessel_eta')
-            eta_date = str(vessel_eta).split()[0]
-            combined = f"{vessel_name} → {eta_date}"
+
             if vessel_name and vessel_name not in vessel_names:
                 vessel_names.append(vessel_name)
-                vessel_combined.append(combined)
+                eta_date = str(vessel_eta).split()[0] if vessel_eta else "未知"
+                vessel_dict[vessel_name] = f"{vessel_name} → {eta_date}"
         context = {
             'warehouse': warehouse,
             'warehouse_options': self.warehouse_options,
@@ -1529,7 +1529,7 @@ class PostNsop(View):
             'max_cbm': max_cbm,
             'max_pallet': max_pallet,
             "vessel_names": vessel_names,
-            "vessel_combined": vessel_combined,
+            "vessel_dict": vessel_dict,
         }
         return self.template_main_dash, context
     
