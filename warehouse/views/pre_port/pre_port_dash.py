a33797cc686456e6e11c1d7f3fcacb472a24aa2e
qyj <2369385394@qq.com>
2025-11-03 16:58:57 +0800

    12.在货柜状态追踪，按照还空相同模式，添加一个批量录入拆柜数据回传时间，此数据可同步显示在货柜进度汇总的拆柜状态一栏（改名为拆柜时间）

--- warehouse/views/pre_port/pre_port_dash.py
+++ warehouse/views/pre_port/pre_port_dash.py
@@ -67,6 +67,9 @@ class PrePortDash(View):
         elif step == "get_offload_at":
             template, context = await self.get_offload_at(request)
             return await sync_to_async(render)(request, template, context)
+        elif step == "batch_get_offload_at":
+            template, context = await self.batch_get_offload_at(request)
+            return await sync_to_async(render)(request, template, context)
 
         else:
             return await sync_to_async(render)(request, self.template_main, {})
@@ -240,6 +243,37 @@ class PrePortDash(View):
         end_date = request.POST.get("end_date", None)
         start_date_eta = request.POST.get("start_date_eta", None)
         end_date_eta = request.POST.get("end_date_eta", None)
+        template, context = await self.handle_all_get(
+            start_date=start_date,
+            end_date=end_date,
+            start_date_eta=start_date_eta,
+            end_date_eta=end_date_eta,
+            tab="summary",
+        )
+        return template, context
+
+    async def batch_get_offload_at(self, request: HttpRequest) -> tuple[Any, Any]:
+        offload_ids = request.POST.getlist("offload_ids[]")
+        time_str = request.POST.get("offload_at")
+        if offload_ids:
+            for offload_id in offload_ids:
+                try:
+                    offload_obj = await Offload.objects.aget(offload_id=offload_id)
+                    if time_str:
+                        naive_datetime = datetime.strptime(time_str, "%Y-%m-%dT%H:%M")
+                        aware_datetime = timezone.make_aware(naive_datetime)
+                        offload_obj.offload_at = aware_datetime
+                    else:
+                        offload_obj.offload_at = None
+                    await offload_obj.asave()
+                except Offload.DoesNotExist:
+                    continue
+
+        start_date = request.POST.get("start_date", None)
+        end_date = request.POST.get("end_date", None)
+        start_date_eta = request.POST.get("start_date_eta", None)
+        end_date_eta = request.POST.get("end_date_eta", None)
+
         template, context = await self.handle_all_get(
             start_date=start_date,
             end_date=end_date,
