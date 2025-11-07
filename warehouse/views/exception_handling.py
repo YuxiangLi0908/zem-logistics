4ffbc5a3e7ec4800d1911aadad7aed370614abc5
sangwei <1178424577@qq.com>
2025-11-07 15:54:36 +0800

    1

--- warehouse/views/exception_handling.py
+++ warehouse/views/exception_handling.py
@@ -192,6 +192,7 @@ class ExceptionHandling(View):
         context = {}
         search_date_str = request.POST.get('search_date')
         search_date_lower_str = request.POST.get('search_date')
+        
         search_date = datetime.fromisoformat(search_date_str)      
         search_date_lower = datetime.fromisoformat(search_date_lower_str)   
         # 查询条件：指定时间之前，shipment_batch_number为空
