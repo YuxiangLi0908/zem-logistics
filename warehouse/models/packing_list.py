ffa552eed4bca09f54678db4a18f39365ec7f897
qyj <2369385394@qq.com>
2025-12-04 09:27:47 +0800

    packing_list的编码长度

--- warehouse/models/packing_list.py
+++ warehouse/models/packing_list.py
@@ -19,7 +19,7 @@ class PackingList(models.Model):
     fba_id = models.CharField(max_length=400, null=True, blank=True)
     destination = models.CharField(max_length=255, null=True, blank=True)
     address = models.CharField(max_length=2000, null=True, blank=True)
-    zipcode = models.CharField(max_length=20, null=True, blank=True)
+    zipcode = models.CharField(max_length=200, null=True, blank=True)
     contact_name = models.CharField(max_length=255, null=True, blank=True)
     contact_method = models.CharField(max_length=400, null=True, blank=True)
     ref_id = models.CharField(max_length=400, null=True, blank=True)
