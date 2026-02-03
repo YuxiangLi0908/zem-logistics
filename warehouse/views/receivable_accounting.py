976321302c9807e43454096410e209de9b313cc1
sangwei <1178424577@qq.com>
2026-02-03 17:39:44 +0800

    1

--- warehouse/views/receivable_accounting.py
+++ warehouse/views/receivable_accounting.py
@@ -2798,7 +2798,7 @@ class ReceivableAccounting(View):
         all_invoices = Invoicev2.objects.filter(
             container_number_id__in=container_ids
         ).prefetch_related(status_prefetch)
-
+        print('所有的账单',all_invoices)
         # --- 5. [安全机制] 批量创建缺失的 InvoiceStatus ---
         missing_statuses = []
         invoices_needing_update = []
@@ -2922,6 +2922,7 @@ class ReceivableAccounting(View):
             "default_tab": default_tab, 
             "is_leader": is_leader,
             "container_ids": container_ids,
+            "all_invoices": all_invoices,
         })
         return context
 
