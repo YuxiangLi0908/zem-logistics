12142954988c1a746a3ea840260bc952f459a687
yuxiang <yuxiangli.0908@gmail.com>
2026-05-02 09:00:36 -0400

    add default value to n_label

--- warehouse/views/post_port/warehouse/palletization.py
+++ warehouse/views/post_port/warehouse/palletization.py
@@ -1738,7 +1738,7 @@ class Palletization(View):
         else:
             customer_name = request.POST.get("customer_name")
             status = request.POST.get("status")
-            n_label = int(request.POST.get("n_label"))
+            n_label = int(request.POST.get("n_label", 4))
             retrieval = await sync_to_async(Retrieval.objects.get)(
                 order__container_number__container_number=container_number
             )
