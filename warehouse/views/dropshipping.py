be67405c6cca87064cda4a9d9f8440c946fd95f2
qyj <2369385394@qq.com>
2026-06-25 11:30:37 +0800

    1

---
+++ warehouse/views/dropshipping.py
@@ -0,0 +1,14 @@
+from django.http import HttpRequest, HttpResponse
+from django.views import View
+
+
+class Dropshipping(View):
+    def get(self, request: HttpRequest, **kwargs) -> HttpResponse:
+        step = request.GET.get("step", None)
+        if step == "warehouse":
+            pass
+
+    def post(self, request: HttpRequest, **kwargs) -> HttpRequest:
+        step = request.POST.get("step")
+        if step == "warehouse":
+            pass
