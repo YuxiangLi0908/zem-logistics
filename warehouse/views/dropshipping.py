9306cb7882a319205519bac9dbc9051cdc6af076
yuxiang <yuxiangli.0908@gmail.com>
2026-09-25 13:23:25 -0400

    add model to shipping mark for selected customer

--- warehouse/views/dropshipping.py
+++ warehouse/views/dropshipping.py
@@ -1918,8 +1918,8 @@ class Dropshipping(View):
                     else:
                         new_marks = marks + "TTT1"
 
-                if customer_name in ["JINYU", "FWE"]:
-                    new_marks = f"{model}_{new_marks}" if new_marks else None
+                # if customer_name in ["JINYU", "FWE"]:
+                new_marks = f"{model}_{new_marks}" if new_marks else None
 
                 # 生成后 强制截断唛头
                 if new_marks:
